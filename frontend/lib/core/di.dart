a8a833dcf3e5da05b3fee8e1bcd26b9ea275c800
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2022-09-21 11:32:17 +0200

    By default, fingers crossed, you have backend running on localhost:3000

--- frontend/lib/core/di.dart
+++ frontend/lib/core/di.dart
@@ -6,7 +6,10 @@ final GetIt getIt = GetIt.instance;
 void configureDependencies() {
   final provider = ApiProvider(
     groupID: const String.fromEnvironment('ID', defaultValue: 'test'),
-    baseHostname: const String.fromEnvironment('URL'),
+    baseHostname: const String.fromEnvironment(
+      'URL',
+      defaultValue: 'localhost:3000',
+    ),
   );
   getIt.registerSingleton<IApiProvider>(provider);
 }
